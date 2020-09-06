ab25a0506c6b748e2283240c7308ad61ce7f1c47
cartoon-raccoon <shaughn.chan@gmail.com>
2020-09-06 16:10:50 +0800

    Refactored App struct to mod file

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -8,11 +8,22 @@ use message::Message;
 use crate::collection::Collection;
 use crate::wantlist::Wantlist;
 
-//#[derive(Debug, Copy)]
+//#[derive(Debug, Clone)]
 pub struct App{
     pub user_id: String,
     pub token: String,
     pub message: Message,
     pub collection: Collection,
     pub wantlist: Wantlist,
+}
+
+#[derive(Debug, Clone)]
+pub struct Release {
+    pub id: u64,
+    pub title: String,
+    pub artist: String,
+    pub year: u64,
+    pub labels: Vec<String>,
+    pub formats: Vec<String>,
+    pub date_added: String,
 }
