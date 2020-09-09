be6766566c39bd7cdc0e501f7bed6fa080033f8c
cartoon-raccoon <shaughn.chan@gmail.com>
2020-09-10 00:28:40 +0800

    Implemented update module of database API
    The update module of the database API is (almost) complete. The provided functions can write to the database without any errors, and those that do occur are caught.
    The remaining functions to implement are the ones that insert logs and update the user id and token, but those should be easy to implement.
    
    The next module to implement is the query module, and the rest of the admin module (check integrity and purge folders).

--- src/app/database.rs
+++ src/app/database.rs
@@ -17,13 +17,13 @@ pub mod admin {
     //database initialization
     //this should only be called on first time startup
     //or when the database has been purged
-    pub fn init_db(folderlist: Vec<String>) -> Result<(), Box<dyn Error>> {
+    pub fn init_db() -> Result<(), Box<dyn Error>> {
         let conn = Connection::open("cogsy_data.db")?;
         conn.execute(
             "CREATE TABLE IF NOT EXISTS profile (
                 username TEXT PRIMARY KEY,
                 real_name TEXT,
-                registered BOOLEAN,
+                registered TEXT,
                 listings INTEGER,
                 collection INTEGER,
                 wantlist INTEGER,
@@ -39,8 +39,8 @@ pub mod admin {
                 title TEXT NOT NULL,
                 artist TEXT,
                 year INTEGER,
-                labels BLOB,
-                formats BLOB,
+                labels TEXT,
+                formats TEXT,
                 date_added TEXT
             )",
             NO_PARAMS
@@ -53,8 +53,13 @@ pub mod admin {
             NO_PARAMS
         )?;
 
-        for foldername in folderlist {
-            let sqlcommand = format!("CREATE TABLE IF NOT EXISTS {} (
+        Ok(())
+    }
+
+    pub fn init_folder(foldername: String, conn: &Connection) 
+        -> Result <(), Box<dyn Error>> {
+        let sqlcommand = format!(
+            "CREATE TABLE IF NOT EXISTS {} (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
                 artist TEXT,
@@ -64,42 +69,129 @@ pub mod admin {
                 date_added TEXT
             )", foldername);
 
-            conn.execute(&sqlcommand, NO_PARAMS)?;
-        }
+        conn.execute(&sqlcommand, NO_PARAMS)?;
         Ok(())
     }
 }
 
 pub mod update {
     use std::error::Error;
-    use rusqlite::Connection;
+    use rusqlite::{
+        Connection,
+        NO_PARAMS
+    };
     use crate::app::{
         Release, 
         Folders, 
         Profile
     };
+    use crate::app::database::admin;
 
-    pub fn profile(profile: Profile) -> Result<(), ()> {
-        let conn = Connection::open("cogsy_data.db");
+    pub fn profile(profile: Profile) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open("cogsy_data.db")?;
+        conn.execute("DELETE FROM profile;", NO_PARAMS)?;
+        conn.execute("INSERT INTO profile 
+        (username, 
+        real_name, 
+        registered, 
+        listings, 
+        collection, 
+        wantlist,
+        rated,
+        average_rating) VALUES
+        (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8);", 
+        &[
+            &profile.username,
+            &profile.real_name,
+            &profile.registered,
+            &profile.listings.to_string(),
+            &profile.wantlist.to_string(),
+            &profile.collection.to_string(),
+            &profile.rated.to_string(),
+            &profile.average_rating.to_string()
+        ])?;
         Ok(())
     }
 
-    pub fn collection(collection: Folders) -> Result<usize, Box<dyn Error>> {
-        Ok(0)
+    pub fn collection(mut collection: Folders) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open("cogsy_data.db")?;
+        for (name, folder) in collection.contents.iter_mut() {
+            let mut sanitized_name = name.clone();
+            sanitized_name.push_str("_");
+            admin::init_folder(sanitized_name.clone(), &conn)?;
+            add_release(&conn, &sanitized_name, folder)?;
+        }
+        Ok(())
     }
 
-    pub fn wantlist(wantlist: Vec<Release>) -> Result<usize, Box<dyn Error>> {
-        Ok(0)
+    pub fn wantlist(mut wantlist: Vec<Release>) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open("cogsy_data.db")?;
+        conn.execute("DELETE FROM wantlist;", NO_PARAMS)?;
+        add_release(&conn, "wantlist", &mut wantlist)?;
+        Ok(())
+    }
+
+    fn add_release(conn: &Connection, foldername: &str, folder: &mut Vec<Release>)
+        -> Result<(), Box<dyn Error>> {
+        for release in folder {
+            let mut stringified_labels = String::new();
+            for label in &release.labels {
+                stringified_labels.push_str(&label);
+                stringified_labels.push(':');
+            }
+
+            let mut stringified_formats = String::new();
+            for format in &release.formats {
+                stringified_formats.push_str(&format);
+                stringified_formats.push(':');
+            }
+
+            stringified_labels.pop().unwrap();
+            stringified_formats.pop().unwrap();
+
+            let mut stmt = conn.prepare(
+                &format!("INSERT INTO {}
+                (id,
+                title,
+                artist,
+                year,
+                labels,
+                formats,
+                date_added) VALUES
+                (?1, ?2, ?3, ?4, ?5, ?6, ?7);", 
+                foldername)
+            )?;
+            stmt.execute(
+                &[
+                    &release.id.to_string(),
+                    &release.title,
+                    &release.artist,
+                    &release.year.to_string(),
+                    &stringified_labels,
+                    &stringified_formats,
+                    &release.date_added,
+                ]
+            )?;
+        }
+        Ok(())
     }
 }
 
 pub mod query {
+    use std::error::Error;
     use rusqlite;
     use crate::app::{
         Release, 
         Folders, 
         Profile
     };
+
+    /*
+    profile(), collection() and wantlist() are called when the app starts
+    they must not fail, so they don't have to return a result
+    ideally, admin::check() should properly ensure database integrity before starting the app.
+    however, it cannot protect against the contents of the table itself.
+    */
 
     pub fn profile() {
 
@@ -114,7 +206,7 @@ pub mod query {
     }
 
     //returns a vec of releases to support multiple results
-    pub fn release(title: String) -> Vec<Release> {
-        Vec::new()
+    pub fn release(title: String) -> Result<Vec<Release>, Box<dyn Error>> {
+        Ok(Vec::new())
     }
 }
