90bd91ef6248b82101d0dda45f48d71961d4ff87
cartoon-raccoon <shaughn.chan@gmail.com>
2020-09-09 20:56:06 +0800

    Added database initialization fn
    database.rs now contains the new admin module, for general functions like checking the database integrity or initializing the database.
    These functions will be called on app startup to check that the required tables are there, and the app will throw up an error message and refuse to start if the checks fail.
    
    As of now the module only contains the init_db function to create the database on first time startup or after a purge, but functions to check integrity and purge the db will be added.

--- src/app/database.rs
+++ src/app/database.rs
@@ -1,23 +1,87 @@
 /*
 *the database API exposes two main modules:
 *   mod update: called by the request module query() to load in data
-*       fns: update_db, load_into_db
 *       also exposes functions that update userid and token
+*       and log listening history
 *   mod query: queries from the local database
-*       IMPORTANT: due to the way data is added to the screens,
-*       every query must return an iterator or indexable set of iterators
+*       every query returns an iterator or indexable set of iterators
+*       wrapped in a custom type (Folders, Release)
 */
+pub mod admin {
+    use std::error::Error;
+    use rusqlite::{
+        Connection,
+        NO_PARAMS,
+    };
+
+    //database initialization
+    //this should only be called on first time startup
+    //or when the database has been purged
+    pub fn init_db(folderlist: Vec<String>) -> Result<(), Box<dyn Error>> {
+        let conn = Connection::open("cogsy_data.db")?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS profile (
+                username TEXT PRIMARY KEY,
+                real_name TEXT,
+                registered BOOLEAN,
+                listings INTEGER,
+                collection INTEGER,
+                wantlist INTEGER,
+                rated INTEGER,
+                average_rating REAL
+            )",
+            NO_PARAMS
+        )?;
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS wantlist (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                artist TEXT,
+                year INTEGER,
+                labels BLOB,
+                formats BLOB,
+                date_added TEXT
+            )",
+            NO_PARAMS
+        )?;
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS folders (
+                name TEXT PRIMARY KEY
+            )",
+            NO_PARAMS
+        )?;
+
+        for foldername in folderlist {
+            let sqlcommand = format!("CREATE TABLE IF NOT EXISTS {} (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                artist TEXT,
+                year INTEGER,
+                labels BLOB,
+                formats BLOB,
+                date_added TEXT
+            )", foldername);
+
+            conn.execute(&sqlcommand, NO_PARAMS)?;
+        }
+        Ok(())
+    }
+}
+
 pub mod update {
     use std::error::Error;
-    use rusqlite;
+    use rusqlite::Connection;
     use crate::app::{
         Release, 
         Folders, 
         Profile
     };
 
-    pub fn profile(profile: Profile) -> Result<usize, Box<dyn Error>> {
-        Ok(0)
+    pub fn profile(profile: Profile) -> Result<(), ()> {
+        let conn = Connection::open("cogsy_data.db");
+        Ok(())
     }
 
     pub fn collection(collection: Folders) -> Result<usize, Box<dyn Error>> {
