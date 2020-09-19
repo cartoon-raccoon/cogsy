fc1956594db20756ca9821ca491fa7ed11157f8f
cartoon-raccoon <shaughn.chan@gmail.com>
2020-09-19 23:43:17 +0800

    Changed version no to 0.1.2

--- src/app/impl_app.rs
+++ src/app/impl_app.rs
@@ -97,7 +97,7 @@ impl App {
             user_id: config.username,
             token: token,
             message: Message {
-                msg: String::from("Cogsy v0.1.1"),
+                msg: String::from("Cogsy v0.1.2"),
                 kind: MessageKind::Info
             },
             collection: Collection::new(),
