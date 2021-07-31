9baa1a53633aeeb0d1249a9ed76f87b630e4df6d
cartoon-raccoon <shaughn.chan@gmail.com>
2021-07-31 14:52:21 +0800

    Added wantlist validation

--- src/app/csv.rs
+++ src/app/csv.rs
@@ -4,7 +4,7 @@ use csv::{Reader, StringRecord};
 use unidecode::unidecode;
 use chrono::{DateTime, Utc};
 
-use crate::app::{request::UpdateError, Release};
+use crate::app::{request::UpdateError, Release, Folders};
 use crate::utils;
 use crate::CONFIG;
 
@@ -46,6 +46,7 @@ const FORMAT:      usize = 4;
 const RATING:      usize = 5;
 const RELEASED:    usize = 6;
 const RELEASE_ID:  usize = 7;
+const WANT_NOTES:  usize = 8;
 const COL_FOLDER:  usize = 8;
 const DATE_ADDED:  usize = 9;
 const MEDIA_COND:  usize = 10;
@@ -76,10 +77,10 @@ impl Release {
     }
 }
 
-pub fn parse_csv<P: AsRef<Path>>(path: P) -> Result<Vec<Release>, UpdateError> {
+pub fn parse_collection_csv<P: AsRef<Path>>(path: P) -> Result<Vec<Release>, UpdateError> {
     let mut reader = Reader::from_path(path)?;
 
-    validate_headers(reader.headers()?)?;
+    validate_coll_headers(reader.headers()?)?;
 
     let mut ret = Vec::new();
     for record in reader.records() {
@@ -89,7 +90,7 @@ pub fn parse_csv<P: AsRef<Path>>(path: P) -> Result<Vec<Release>, UpdateError> {
     Ok(ret)
 }
 
-fn validate_headers(headers: &StringRecord) -> Result<(), UpdateError> {
+fn validate_coll_headers(headers: &StringRecord) -> Result<(), UpdateError> {
     validate!(headers, CATALOG, "Catalog#");
     validate!(headers, ARTIST, "Artist");
     validate!(headers, TITLE, "Title");
@@ -106,6 +107,19 @@ fn validate_headers(headers: &StringRecord) -> Result<(), UpdateError> {
     Ok(())
 }
 
+fn validate_want_headers(headers: &StringRecord) -> Result<(), UpdateError> {
+    validate!(headers, CATALOG, "Catalog#");
+    validate!(headers, ARTIST, "Artist");
+    validate!(headers, TITLE, "Title");
+    validate!(headers, LABEL, "Label");
+    validate!(headers, FORMAT, "Format");
+    validate!(headers, RATING, "Rating");
+    validate!(headers, RELEASED, "Released");
+    validate!(headers, RELEASE_ID, "release_id");
+    validate!(headers, WANT_NOTES, "Notes");
+    Ok(())
+}
+
 #[inline]
 fn vecify(s: &str) -> Vec<String> {
     s.split(',').map(|s| s.trim().to_string()).collect()
@@ -119,6 +133,10 @@ mod tests {
     fn test_csv_header_validation() {
         let mut reader = Reader::from_path("discogs_collection.csv").unwrap();
 
-        validate_headers(reader.headers().unwrap()).unwrap();
+        validate_coll_headers(reader.headers().unwrap()).unwrap();
+
+        let mut reader = Reader::from_path("discogs_wantlist.csv").unwrap();
+
+        validate_want_headers(reader.headers().unwrap()).unwrap();
     }
 }
